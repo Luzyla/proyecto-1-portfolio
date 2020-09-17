31891a34877bda9f4fdcf32c870ee4438390605f
luzyla <luzyla@gmail.com>
2020-09-17 01:59:58 -0300

    Correcciones menú desplegable mobile

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
+// VARIABLES
 const menuMobile = document.getElementById("icono-burger")
-const menuSeccion = document.getElementById("seccion-menu-mobile")
+const menuSeccion = document.getElementById("menu-mobile")
+const links = document.querySelectorAll("#menu-mobile li")
 
+// DESPLIEGA O CIERRA MENÚ
 menuMobile.onclick = () => {
-    menuSeccion.classList.toggle("menu-oculto")
+    menuSeccion.classList.toggle("menu-desplegado")
+}
+
+// CIERRA MENÚ AL IR A LINK
+for (let link of links) {
+    link.onclick = () => {
+        menuSeccion.classList.remove("menu-desplegado")
+    }
 }
