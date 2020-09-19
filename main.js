d5dec754e5f751dab820a1d20fcd425ccef6f867
luzyla <luzyla@gmail.com>
2020-09-19 03:56:29 -0300

    Filtro en sección Proyectos

--- main.js
+++ main.js
@@ -14,3 +14,34 @@ for (let link of links) {
         menuSeccion.classList.remove("menu-desplegado")
     }
 }
+
+// FILTRO PROYECTOS
+const buttons = document.querySelectorAll(".menu-proyectos")
+const proyectos = document.querySelectorAll(".proyectos")
+
+// console.log(buttons)
+// console.log(proyectos)
+
+for (let button of buttons) {
+    
+    button.onclick = () => {
+        for (let proyecto of proyectos) {
+            const boton = button.dataset.name
+            const proyectoFiltrado = proyecto.dataset.name
+                        
+            if (boton === proyectoFiltrado) {
+                proyecto.classList.remove("hidden")
+            }
+            else if (boton === "todos") {
+                proyecto.classList.remove("hidden")
+            }
+            else {
+                proyecto.classList.add("hidden")
+            }
+        }
+    
+    }
+
+}       
+
+
